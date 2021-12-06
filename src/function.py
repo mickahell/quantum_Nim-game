e60e0890b169dcf2dba24bb92978857df10d9dfd
Michaël <mika89700@gmail.com>
2021-12-06 14:03:42 +0100

    Update function.py

--- src/function.py
+++ src/function.py
@@ -10,15 +10,12 @@ def control_input(options: int, input_ctl: int) -> bool:
         if isinstance(input_ctl, int):
             if 0 < input_ctl <= 3:
                 return True
-            elif options + 1 < 3:
-                print("Please give a number between 0 and {}.".format(options))
-                return False
             else:
-                print("Please give a number between 0 and {}.".format(3))
+                print("Please give a number between 0 and {}.".format(options))
                 return False
         else:
             if input_ctl in options:
                 return True
             else:
                 print("Please give a valid gate between {}".format(options))
-    return False
+                return False
